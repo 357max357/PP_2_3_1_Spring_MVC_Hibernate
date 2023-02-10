20e516b66987b0381182775f4c70a2d283b001fe
MaximAntsiferov357 <maxim.a@wildnorth.games>
2023-02-10 10:46:02 +0300

    Moved @Transactional to Userserviceimpl.java

--- src/main/java/web/dao/UserDaoImp.java
+++ src/main/java/web/dao/UserDaoImp.java
@@ -5,7 +5,6 @@ import web.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -15,34 +14,29 @@ public class UserDaoImp implements UserDao {
     private EntityManager entityManager;
 
     @Override
-    @Transactional
     public List<User> getAllUsers() {
         return entityManager.createQuery("from User").getResultList();
     }
 
     @Override
-    @Transactional
     public void addUser(User user) {
         entityManager.persist(user);
         entityManager.flush();
     }
 
     @Override
-    @Transactional
     public void deleteUser(User user) {
         entityManager.remove(entityManager.contains(user) ? user : entityManager.merge(user));
         entityManager.flush();
     }
 
     @Override
-    @Transactional
     public void updateUser(User user) {
         entityManager.merge(user);
         entityManager.flush();
     }
 
     @Override
-    @Transactional
     public User getById(long id) {
         return entityManager.find(User.class, id);
     }
--- src/main/java/web/service/UserServiceImp.java
+++ src/main/java/web/service/UserServiceImp.java
@@ -1,13 +1,15 @@
 package web.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import web.dao.UserDao;
 import web.model.User;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
 public class UserServiceImp implements UserService {
 
     private final UserDao userDao;
@@ -23,16 +25,19 @@ public class UserServiceImp implements UserService {
     }
 
     @Override
+    @Transactional
     public void addUser(User user) {
         userDao.addUser(user);
     }
 
     @Override
+    @Transactional
     public void deleteUser(User user) {
         userDao.deleteUser(user);
     }
 
     @Override
+    @Transactional
     public void updateUser(User user) {
         userDao.updateUser(user);
     }
